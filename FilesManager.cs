bff91644e0c0dfa0d030ac98e7fc5d6b00e37487
victoor83 <44602692+victoor83@users.noreply.github.com>
2025-11-06 13:25:45 +0100

    Add status label with last saved time and information if last work day has been stored

---
+++ FilesManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace TimeUpdater
+{
+    public class FilesManager
+    {
+        private const string _CONFIG_FILE = "config.json";
+
+        public FilesManager()
+        {
+            FilePaths = LoadConfigFile();
+        }
+
+        public Paths FilePaths { get; }
+
+        public bool SaveTimesForSingleDay(List<double> dateTimes, string projectName)
+        {
+            try
+            {
+                CreateBackupFile();
+
+                //Save new times to broker file
+                List<string> lines = File.ReadAllLines(FilePaths.BrokerTimeFile).ToList();
+
+                int i = 0;
+                foreach(var dateTime in dateTimes)
+                {
+                    //0 - start work  1 - start break  2- end break (start work time)  3 - end workday
+                    TimeType timeType = (i == 0 || i == 2) ? TimeType.Start : TimeType.Stop;
+
+                    lines.Insert(lines.Count - 1, GetTimeLine(dateTime, timeType, projectName));
+
+                    i++;
+                }
+
+                File.WriteAllLines(FilePaths.BrokerTimeFile, lines);
+
+                //Copy broker file to WordTimeRecorder
+                File.Copy(FilePaths.BrokerTimeFile, FilePaths.TimeFilePath, true);
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get last saved time of work day as DateTime 
+        /// </summary>
+        /// <returns></returns>
+        public DateTime? GetLastSavedTime()
+        {
+            var unixTime = GetLastSavedTimeUnixFormat();
+            if(unixTime > 0)
+            {
+                return TimeConverter.ConvertUnixTimeToDateTime(unixTime);
+            }
+
+            return null;
+        }
+
+        private int GetLastSavedTimeUnixFormat()
+        {
+            if(!File.Exists(FilePaths.BrokerTimeFile))
+            {
+                MessageBox.Show($"File '{FilePaths.BrokerTimeFile}' doesn't exists.");
+                return -1;
+            }
+
+            var lastLine = File.ReadLines(FilePaths.BrokerTimeFile).TakeLast(2).SkipLast(1).First();
+            //Get number (unix time)  from string
+            var unixTime = Convert.ToInt32(new string(lastLine.Where(char.IsDigit).ToArray()));
+
+            return unixTime;
+        }
+
+        private string GetTimeLine(double eventTime, TimeType timeType, string projectType)
+        {
+            return $"<Event time = '{eventTime}' type='{timeType}' description='{projectType}'/>";
+        }
+
+        private void CreateBackupFile()
+        {
+            FileInfo fInfo = new FileInfo(Resources.ResourceManager.GetString("TimeFileName"));
+            var fileName = Path.GetFileNameWithoutExtension(fInfo.Name);
+
+            var timeFile = Path.Combine(FilePaths.BackupFileFolder, $"{fileName}_{DateTime.Now:dd_MM_yyyy}{fInfo.Extension}");
+            File.Copy(FilePaths.TimeFilePath, Path.Combine(FilePaths.BackupFileFolder, timeFile), true);
+        }
+
+        private Paths LoadConfigFile()
+        {
+            Paths paths = null;
+            // deserialize JSON directly from a file
+            using(StreamReader file = File.OpenText(_CONFIG_FILE))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                paths = (Paths)serializer.Deserialize(file, typeof(Paths));
+            }
+
+            return paths;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TimeUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FilesManager _filesManager;
+
+        private const string _INSERTTIMES_INFO = " Insert 4 Times from excel row";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetDefaultTimes();
+            SetDefaultDate();
+            SetProjects();
+
+            _filesManager = new FilesManager();
+            labFilePath.Content = _filesManager.FilePaths.TimeFilePath;
+            labBackupFolder.Content = _filesManager.FilePaths.BackupFileFolder;
+            labBrokerFilePath.Content = _filesManager.FilePaths.BrokerTimeFile;
+            txtInsertTimes.Text = _INSERTTIMES_INFO;
+
+            SetStatusLabel();
+        }
+
+        private void ButtonAddTime_Click(object sender, RoutedEventArgs e)
+        {
+            var datetime = Dp_Date.SelectedDate;
+            var projectName = comboProject.Text;
+
+            if(datetime == null)
+            {
+                MessageBox.Show("Date is not selected.", "Time error", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            if(string.IsNullOrEmpty(projectName))
+            {
+                MessageBox.Show("Project is not selected.", "Project error", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            var dateTimes = GetDateTimes();
+            var workTime = TimeConverter.CalculateDailyTime(dateTimes);
+
+            if(MessageBox.Show($"Save the date {datetime.Value:dd/MM/yyyy} with total working time {workTime} h ?", "Save these times", MessageBoxButton.YesNo,
+                   MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if(_filesManager.SaveTimesForSingleDay(TimeConverter.ConvertToUnixDateTime(dateTimes), projectName))
+                {
+                    MessageBox.Show($"Success for date {datetime.Value:dd/MM/yyyy}! Total working time is: {workTime} h.", "Info", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Time not saved !");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Canceled!");
+            }
+
+            SetStatusLabel();
+        }
+
+        private void mnuExcel_Click(object sender, RoutedEventArgs e)
+        {
+            FileInfo fi = new FileInfo(_filesManager.FilePaths.ExcelTimeFile);
+            if(fi.Exists)
+            {
+                var p = new Process();
+                p.StartInfo = new ProcessStartInfo(_filesManager.FilePaths.ExcelTimeFile) {UseShellExecute = true,};
+                p.Start();
+            }
+            else
+            {
+                MessageBox.Show($"Excel file with path '{_filesManager.FilePaths.ExcelTimeFile}' given in config file doesn't exist");
+            }
+        }
+
+        private void SetDefaultDate()
+        {
+            DateTime defaultDate = DateTime.Today.AddDays(-1);
+
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                defaultDate = defaultDate.AddDays(-2); //if monday set last friday
+            }
+
+            Dp_Date.SelectedDate = defaultDate;
+        }
+
+        private void SetDefaultTimes()
+        {
+            UcStart.SetCaption("Start");
+            UcStart.cmbHours.Text = 7.ToString();
+            UcStart.cmbMinutes.Text = 0.ToString();
+
+            UcPauseBegin.SetCaption("Pause begin");
+            UcPauseBegin.cmbHours.Text = 13.ToString();
+            UcPauseBegin.cmbMinutes.Text = 0.ToString();
+
+            UcPauseEnd.SetCaption("Pause end");
+            UcPauseEnd.cmbHours.Text = 13.ToString();
+            UcPauseEnd.cmbMinutes.Text = 0.ToString();
+
+            UcEnd.SetCaption("End");
+            UcEnd.cmbHours.Text = 16.ToString();
+            UcEnd.cmbMinutes.Text = 0.ToString();
+        }
+
+        private void SetProjects()
+        {
+            comboProject.ItemsSource = ProjectManager.GetProjectNames();
+        }
+
+        private List<DateTime> GetDateTimes()
+        {
+            //Start
+            DateTime time1 = new DateTime(Dp_Date.SelectedDate.Value.Year, Dp_Date.SelectedDate.Value.Month, Dp_Date.SelectedDate.Value.Day, Convert.ToInt32(UcStart.cmbHours.Text),
+                Convert.ToInt32(UcStart.cmbMinutes.Text), Convert.ToInt32(UcStart.cmbSeconds.Text), DateTimeKind.Local);
+
+            //Pause begin
+            DateTime time2 = new DateTime(Dp_Date.SelectedDate.Value.Year, Dp_Date.SelectedDate.Value.Month, Dp_Date.SelectedDate.Value.Day,
+                Convert.ToInt32(UcPauseBegin.cmbHours.Text), Convert.ToInt32(UcPauseBegin.cmbMinutes.Text), Convert.ToInt32(UcPauseBegin.cmbSeconds.Text), DateTimeKind.Local);
+
+            //Pause end
+            DateTime time3 = new DateTime(Dp_Date.SelectedDate.Value.Year, Dp_Date.SelectedDate.Value.Month, Dp_Date.SelectedDate.Value.Day,
+                Convert.ToInt32(UcPauseEnd.cmbHours.Text), Convert.ToInt32(UcPauseEnd.cmbMinutes.Text), Convert.ToInt32(UcPauseEnd.cmbSeconds.Text), DateTimeKind.Local);
+
+            //End
+            DateTime time4 = new DateTime(Dp_Date.SelectedDate.Value.Year, Dp_Date.SelectedDate.Value.Month, Dp_Date.SelectedDate.Value.Day, Convert.ToInt32(UcEnd.cmbHours.Text),
+                Convert.ToInt32(UcEnd.cmbMinutes.Text), Convert.ToInt32(UcEnd.cmbSeconds.Text), DateTimeKind.Local);
+
+            return new List<DateTime> {time1, time2, time3, time4};
+        }
+
+        private void txtInsertTimes_TextInput(object sender, System.Windows.Input.TextCompositionEventArgs e) { }
+
+        private void txtInsertTimes_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                if(txtInsertTimes.Text != String.Empty && txtInsertTimes.Text != _INSERTTIMES_INFO)
+                {
+                    var arr = txtInsertTimes.Text.Split(" ").Where(x => x != "").ToList();
+                    if(arr.Count == 4)
+                    {
+                        //Start
+                        var hoursMinutes = TimeConverter.ConvertTimeToNumbers(arr[0]);
+                        UcStart.cmbHours.Text = hoursMinutes[0].ToString();
+                        UcStart.cmbMinutes.Text = hoursMinutes[1].ToString();
+
+                        //Pause begin
+                        hoursMinutes = TimeConverter.ConvertTimeToNumbers(arr[1]);
+                        UcPauseBegin.cmbHours.Text = hoursMinutes[0].ToString();
+                        UcPauseBegin.cmbMinutes.Text = hoursMinutes[1].ToString();
+
+                        //Pause end
+                        hoursMinutes = TimeConverter.ConvertTimeToNumbers(arr[2]);
+                        UcPauseEnd.cmbHours.Text = hoursMinutes[0].ToString();
+                        UcPauseEnd.cmbMinutes.Text = hoursMinutes[1].ToString();
+
+                        //End
+                        hoursMinutes = TimeConverter.ConvertTimeToNumbers(arr[3]);
+                        UcEnd.cmbHours.Text = hoursMinutes[0].ToString();
+                        UcEnd.cmbMinutes.Text = hoursMinutes[1].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Times from excel: '" + txtInsertTimes.Text + "' have wrong format", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                        txtInsertTimes.Text = "";
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong", "", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void txtInsertTimes_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtInsertTimes.Text = "";
+        }
+
+        private void txtInsertTimes_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtInsertTimes.Text = _INSERTTIMES_INFO;
+        }
+
+        private void btnOpenFolderFileToWriteInto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolder(labFilePath.Content.ToString());
+        }
+
+        private void btnOpenFolderBackupFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolder(labBackupFolder.Content.ToString());
+        }
+
+        private void btnOpenFolderBrokerFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolder(labBrokerFilePath.Content.ToString());
+        }
+
+        private void OpenFolder(string filePath)
+        {
+            try
+            {
+                string folderPath = Path.GetDirectoryName(filePath);
+                if(folderPath != null)
+                {
+                    Process.Start("explorer.exe", folderPath);
+                }
+                else
+                {
+                    MessageBox.Show($"Path {filePath} is wrong", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show($"Path {filePath} is wrong. Exception message is {e.Message}", "", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void mnuLastTime_Click(object sender, RoutedEventArgs e)
+        {
+            var lastSaveDateTime = _filesManager.GetLastSavedTime();
+            if(lastSaveDateTime.HasValue)
+            {
+                MessageBox.Show(lastSaveDateTime.Value.ToString(CultureInfo.CurrentCulture), "Last saved time", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void SetStatusLabel()
+        {
+            DateTime? lastSavedDateTemp = _filesManager.GetLastSavedTime();
+
+            if(!lastSavedDateTemp.HasValue)
+            {
+                statusLabel.Content = "Status: No saved date found!";
+                statusLabel.Foreground = Brushes.Red;
+                return;
+            }
+
+            var lastSavedDate = lastSavedDateTemp.Value;
+            DateTime today = DateTime.Today;
+            string statusText;
+            Brush statusColor;
+
+            bool isYesterday = lastSavedDate.Date == today.AddDays(-1);
+            bool isLastSaveOnFriday = today.DayOfWeek == DayOfWeek.Monday && lastSavedDate.Date == today.AddDays(-3) && lastSavedDate.DayOfWeek == DayOfWeek.Friday;
+
+            if(isYesterday || isLastSaveOnFriday)
+            {
+                statusText = $"Status: OK  (last saved date: {lastSavedDate:yyyy-MM-dd})";
+                statusColor = Brushes.Green;
+            }
+            else
+            {
+                statusText = $"Status: Last workday is potentially not stored! (last saved date: {lastSavedDate:yyyy-MM-dd})";
+                statusColor = Brushes.Red;
+            }
+
+            statusLabel.Content = statusText;
+            statusLabel.Foreground = statusColor;
+        }
+    }
+}
